46b9906fa2b0c3769f70ee159c939061080a0cff
saurabhs <saurabh.shendye@gmail.com>
2017-12-26 16:51:11 -0700

    Chat application implemented (Push notifications yet to be implemented)

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 var express = require('express')
-
+var bodyParser = require('body-parser')
 var app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+ }));
+ app.use(express.static(__dirname))
+
+
+var messages = [
+
+]
+
+
+// app.get('/essages', )
+app.get('/messages', (req, res) =>{
+    console.log('in get:', messages)
+    res.send(messages)
+})
+
+app.post('/messages', (req, res)=>{
+    console.log('Message Posted: ', req.body)
+    messages.push(req.body)
+    res.sendStatus(200)
+})
+
 var server = app.listen(5001, () => {
-    console.log('Server listnening on port: ', server.address().port);
+    console.log('Server listnening on port:', server.address().port);
 })
